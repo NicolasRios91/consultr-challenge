09bbefdcbc19cd47c6698f15907c34c2caa9ff1d
riosnicolass <nicolasrios91@hotmail.com>
2022-12-09 19:55:48 -0300

    feat: pagination

---
+++ app/src/components/button/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+  padding: 10px;
+  background-color: #87ceec;
+  border: 2px solid #87ceec;
+  box-sizing: border-box;
+  border-radius: 6px;
+  cursor: pointer;
+
+  :hover {
+    border: 2px solid #000000;
+  }
+`;
--- app/src/views/landing/index.jsx
+++ app/src/views/landing/index.jsx
@@ -5,15 +5,25 @@ import { useDispatch, useSelector } from "react-redux";
 import { Card } from "../../components/card";
 import { Header } from "../../components/header";
 import { CardsContainer } from "./styles";
+import { StyledButton } from "../../components/button/styles";
 
 export const App = () => {
   const [errorState, setErrorState] = useState({ hasError: false });
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
   const data = useSelector((state) => state.heroes.data);
   const dispatch = useDispatch();
 
   const handleError = (error) => {
     setErrorState({ hasError: true, message: error.message });
+  };
+
+  const handleNextPage = () => {
+    setPage((previous) => previous + 1);
+  };
+
+  const handlePreviousPage = () => {
+    setPage((previous) => previous - 1);
   };
 
   useEffect(() => {
@@ -31,10 +41,27 @@ export const App = () => {
     <>
       <Header />
       <CardsContainer>
-        {data.slice(0, 10).map((character) => (
-          <Card key={character.name} character={character} />
+        {data.slice((page - 1) * 10, page * 10).map((character) => (
+          <Card key={character.id} character={character} />
         ))}
       </CardsContainer>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "10px",
+          padding: "20px",
+        }}
+      >
+        <StyledButton onClick={handlePreviousPage} disabled={page === 1}>
+          Prev
+        </StyledButton>
+        {page}
+        <StyledButton onClick={handleNextPage} disabled={page === data.length}>
+          Next
+        </StyledButton>
+      </div>
     </>
   ) : (
     <div>List is empty</div>
